3de96d88e6bff6d97243fa124f7c3ed6d6ab85a8
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-03-21 18:47:14 +0700

    [HTML_CLIENT] - Trang Blog

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/ui/Header';
 import Footer from './component/ui/Footer';
 // import PageHome from './pages/client/home';
-import Product from './pages/client/product';
+// import Product from './pages/client/product';
+import Blog from './pages/client/blog';
 
 const App = () => {
   return (
     <>
       <Header />
         {/* <PageHome/> */}
-        <Product/>
+        {/* <Product/> */}
+        <Blog/>
       <Footer/>
     </>
   );
