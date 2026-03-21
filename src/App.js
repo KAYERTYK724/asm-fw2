1bd12a51063aa875380f013ab0a6bfb883e52a2b
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-03-22 00:02:26 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/ui/Header';
 import Footer from './component/ui/Footer';
-// import PageHome from './pages/client/home';
+import PageHome from './pages/client/home';
 // import Product from './pages/client/product';
-import ProductDetails from './pages/client/detailProduct';
+// import ProductDetails from './pages/client/detailProduct';
 // import Blog from './pages/client/blog';
 
 const App = () => {
