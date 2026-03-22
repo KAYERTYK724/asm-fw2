1287caa6b20cf55c9a536b582cb2dafc9fdb6db0
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-03-23 00:40:53 +0700

    [HTML_ADMIN] - Trang Dashboard [HTML_ADMIN] - Bổ Sung vào cho nhánh Thiên Tứ

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './component/ui/Header';
-import Footer from './component/ui/Footer';
+// import Header from './component/ui/Header';
+// import Footer from './component/ui/Footer';
 // import PageHome from './pages/client/home';
 // import Product from './pages/client/product';
 // import ProductDetails from './pages/client/detailProduct';
 // import Blog from './pages/client/blog';
-import BlogDetails from './pages/client/detailBlog';
+// import BlogDetails from './pages/client/detailBlog';
+
+// import HeaderAdmin from './component/admin/Header';
+// import FooterAdmin from './component/admin/Footer';
+// import Dashboard from './pages/admin/dashboard';
+
+import AdminLayout from './layout/admin/admin-layout';
 
 const App = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
         {/* <PageHome/> */}
         {/* <Product/> */}
         {/* <ProductDetails/> */}
         {/* <Blog/> */}
-        <BlogDetails/>
-      <Footer/>
+        {/* <BlogDetails/> */}
+      {/* <Footer/> */}
+
+      <AdminLayout/>
     </>
   );
 };
