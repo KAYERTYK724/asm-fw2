02590c7f5ade033bc55a689258e7142b71fb4f91
Hieuntpc08839 <hieuntpc08839@gmail.com>
2026-03-28 18:55:42 +0700

    [HTML_CLIENT] - Trang Contact

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import Product from './pages/client/product';
 import ProductDetails from './pages/client/detailProduct';
 import Blog from './pages/client/blog';
 import BlogDetails from './pages/client/detailBlog';
+import Contact from './pages/client/contact/Contact';
+import Checkout from './pages/client/checkout/Checkout';
+import Cart from './pages/client/cart/Cart';
+import Login  from './pages/client/auth/Login';
+import Register from './pages/client/auth/Register';
 import Dashboard from './pages/admin/dashboard';
 import BlogListAdmin from './pages/admin/blog';
 import AddBlog from './pages/admin/blog/add';
+import OrderList from './pages/admin/orders/OrderList';
+import OrderDetail from './pages/admin/orders/OrderDetail';
 import BlogDetail from './pages/admin/blog/view';
 import ClientLayout from './layout/client/client-layout';
 import AdminLayout from './layout/admin/admin-layout';
@@ -24,6 +31,11 @@ const App = () => {
           <Route path='detailShop' element={<ProductDetails/>}/>
           <Route path='blog' element={<Blog/>}/>
           <Route path='detailBlog' element={<BlogDetails/>}/>
+          <Route path="contact" element={<Contact />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} /> 
         </Route>
         {/* Admin */}
         <Route path='/admin' element={<AdminLayout/>}>
@@ -31,6 +43,9 @@ const App = () => {
           <Route path='blogAdmin' element={<BlogListAdmin/>}/>
           <Route path='addBlog' element={<AddBlog/>}/>
           <Route path='viewBlog' element={<BlogDetail/>}/>
+         
+          <Route path='orders' element={<OrderList/>}/>
+          <Route path='orders/:id' element={<OrderDetail/>}/>
         </Route> 
       </Routes>
     </>
